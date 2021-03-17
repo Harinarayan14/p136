28590735c71cad46d95d606b7c3d53b78a96e17a
Harinarayan14 <62546869+Harinarayan14@users.noreply.github.com>
2021-03-17 18:31:46 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,11 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return jsonify({"data":final_dict,"message":"success"}),200
+@app.route("/star")
+def star():
+    name= request.args.get("name")
+    star_data = next(final_dict[item] for item in final_dict if item == name)
+    print(star_data)
+    return jsonify({"data":star_data,"message":"success"}),200
 if __name__ == "__main__":
     app.run()
